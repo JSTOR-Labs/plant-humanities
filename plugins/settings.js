698a7f2e64780818df465f7ddf954f295ca7b08f
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-03-04 15:58:06 -0500

    GH pages test build

--- plugins/settings.js
+++ plugins/settings.js
@@ -2,16 +2,17 @@ import axios from 'axios'
 import YAML from 'yaml'
 
 export default ({ app }, inject) => {
-    axios.get('/settings.yaml')
-      .then(resp => resp.data)
-      .then(yaml => {
-        const settings = YAML.parse(yaml)
-        console.log('settings', settings)
-        document.title = settings.title
-        app.store.dispatch('setSiteTitle', settings.title )
-        app.store.dispatch('setSiteBanner', settings.banner )
-        app.store.dispatch('setBaseUrl', window.location.hostname === 'localhost' ? window.location.origin : settings.baseurl )
-        app.store.dispatch('setPages', settings.pages )
-        app.store.dispatch('setBundleVersion', settings.lib_version )
-      })
+  console.log(window.location)
+  axios.get('/settings.yaml')
+    .then(resp => resp.data)
+    .then(yaml => {
+      const settings = YAML.parse(yaml)
+      console.log('settings', settings)
+      document.title = settings.title
+      app.store.dispatch('setSiteTitle', settings.title )
+      app.store.dispatch('setSiteBanner', settings.banner )
+      app.store.dispatch('setBaseUrl', window.location.hostname === 'localhost' ? window.location.origin : settings.baseurl )
+      app.store.dispatch('setPages', settings.pages )
+      app.store.dispatch('setBundleVersion', settings.lib_version )
+    })
   }
