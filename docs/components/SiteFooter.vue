058f688b9b46f21e999f7e804c857fb3e4842e39
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-05-06 19:18:28 -0400

    Added site config.

---
+++ docs/components/SiteFooter.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-row>
+      <v-col cols="6" nogutter>
+        <span style="padding-bottom: 8px; font-weight: bold; display: block">A collaboration between:</span>
+        <img src="https://jstor-labs/github.io/plant-humanities/images/do.jpg" height="30px"> &nbsp;&nbsp;&nbsp;&nbsp;
+        <img src="https://jstor-labs/github.io/plant-humanities/images/labs.jpg" height="30px">
+      </v-col>
+      <v-col cols="6">
+        <span style="padding-bottom: 8px; font-weight: bold; display: block">Funded by:</span>
+        <img src="https://jstor-labs/github.io/plant-humanities/images/Mellon.jpg" height="30px">
+      </v-col>
+    </v-row>
+</template>
+
+<script>
+  module.exports = {  
+    data: () => ({})
+  }
+</script>
+
+<style>
+  
+  [v-cloak] { display: none; }
+  
+  .site-footer, .row {
+    padding: 0;
+    margin: 0;
+  }
+  .col {
+    padding: 0 16px;
+  }
+
+</style>
