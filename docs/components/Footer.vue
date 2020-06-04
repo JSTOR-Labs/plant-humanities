a982b0e3b5df7948784aee1aea61016e287a90d9
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-06-04 16:56:13 -0400

    Updated DO logo.

--- docs/components/Footer.vue
+++ docs/components/Footer.vue
@@ -3,7 +3,7 @@
     <v-row>
       <v-col cols="6" nogutter>
         <span style="padding-bottom: 8px; font-weight: bold; display: block">A collaboration between:</span>
-        <img src="https://jstor-labs.github.io/plant-humanities/images/do.jpg" height="30px"> &nbsp;&nbsp;&nbsp;&nbsp;
+        <img src="https://jstor-labs.github.io/plant-humanities/images/do-logo.svg" height="30px"> &nbsp;&nbsp;&nbsp;&nbsp;
         <img src="https://jstor-labs.github.io/plant-humanities/images/labs.jpg" height="30px">
       </v-col>
       <v-col cols="6">
