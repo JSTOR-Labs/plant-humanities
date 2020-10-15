f60550eaf253814cef21e2623d8381a68914112b
Ron Snyder <Ronald.Snyder@ithaka.org>
2020-10-15 08:48:07 -0400

    Update to config and footer.

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,54 +1,37 @@
 <template>
-  <v-container id="footer" ref="footer" v-mutate.attr="onMutate" style="z-index:100 !important;">
-    <v-row>
-      <v-col cols="6" nogutter>
-        <span style="padding-bottom: 8px; font-weight: bold; font-size: 14px;">A collaboration between:</span>
-        <img src="https://jstor-labs.github.io/plant-humanities/images/do-logo.svg" style="height:100px; margin:-40px 0;"> &nbsp;&nbsp;&nbsp;&nbsp;
-        <img src="https://jstor-labs.github.io/plant-humanities/images/labs.jpg" height="20px">
-      </v-col>
-      <v-col cols="6">
-        <span style="padding-bottom: 8px; font-weight: bold; font-size: 14px;">Funded by:</span>
-        <img src="https://jstor-labs.github.io/plant-humanities/images/Mellon.jpg" height="20px">
-      </v-col>
-    </v-row>
-  </v-container>
+  <div id="footer">
+    <img src="https://jstor-labs.github.io/plant-humanities/images/do-logo.svg" style="height:100px; margin:-40px 0;"> &nbsp;&nbsp;&nbsp;&nbsp;
+    <img src="https://jstor-labs.github.io/plant-humanities/images/labs.jpg" height="20px">
+    <img src="https://jstor-labs.github.io/plant-humanities/images/Mellon.jpg" height="20px">
+  </div>
 </template>
 
 <script>
+
   module.exports = {  
-    data: () => ({
-      height: undefined
-    }),
-    mounted() {
-      this.height = this.$refs.footer.clientHeight
-      this.$emit('footer-height', this.height)
-    },
-    methods: {
-      onMutate(mutations) {
-        const mutation = mutations[mutations.length - 1]
-        if (mutation.target && mutation.target.clientHeight !== this.height) {
-          this.height = mutation.target.clientHeight
-          this.$emit('footer-height', this.height)
-        }
-      }
-    }
+    name: 'Footer',
+    props: {},
+    data: () => ({}),
+    computed: {},
+    mounted() {},
+    methods: {},
+    watch: {}
   }
+
 </script>
 
 <style>
   
   [v-cloak] { display: none; }
+
   #footer {
-    border: 1px solid #ddd;
-    margin: 0;
-    max-width: none;
+    display: flex;
+    align-items: center;
+    height: 50px;
   } 
-  .site-footer, .row {
-    padding: 0;
-    margin: 0;
-  }
-  .col {
-    padding: 0 16px;
+  #footer img {
+    height: 30px;
+    margin: 0 12px;
   }
 
 </style>
