4df8d954771a19579e4ed011880e9127fd8ce6a7
Ron Snyder <ron@snyderjr.com>
2021-02-02 11:35:41 -0500

    Map inventory formatting

--- scripts/images-inventory.py
+++ scripts/images-inventory.py
@@ -10,19 +10,28 @@ import sys
 
 from bs4 import BeautifulSoup
 
+fields = ['ready', 'essay', 'thumbnail', 'manifest', 'height', 'width', 'format', 'iiif-url', 'attribution', 'source', 'author', 'description', 'license', 'label', 'url', 'attribution-url']
+field_names_map = {'title': 'label'}
+
 if __name__ == '__main__':
 
     rootDir = sys.argv[1] if len(sys.argv) > 1 else '.'
-
+    
+    print('\t'.join(fields))
     for dirName, subdirList, fileList in os.walk(rootDir):
         for fname in fileList:
             if fname.endswith('.md'):
                 essay = fname[:0-3]
+                if essay == 'README':
+                    essay = dirName.split('/')[-1]
+                if essay not in ('cinnamon', 'sunflower', 'pelargonium'): continue
                 html = open(f'{dirName}/{fname}', 'r').read()
                 soup = BeautifulSoup(html, 'html5lib')
                 for tag in soup.find_all('param'):
                     if 've-image' in tag.attrs:
-                        rec = [essay]
-                        for attr in ('attribution', 'description', 'iiif-url', 'license', 'manifest', 'title', 'url'):
-                            rec.append(tag.attrs[attr] if attr in tag.attrs else '')
+                        data = {'essay': essay}
+                        for attr in ('attribution', 'description', 'iiif-url', 'label', 'license', 'manifest', 'title', 'url'):
+                            if attr in tag.attrs:
+                                data[field_names_map.get(attr, attr)] = tag.attrs[attr].replace('\t', ' ').replace('\n', ' ')
+                        rec = [data.get(f,'') for f in fields]
                         print('\t'.join(rec))
