cce841d35c607b27532c780c953a7f4d27f86189
Ron Snyder <ronald.snyder@ithaka.org>
2021-08-30 10:42:32 -0400

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,47 +1,83 @@
 <template>
   <div id="footer" :style="`max-width:${maxWidth}px; overflow:hidden;`">
-    a collaboration between <img :src="`${assetsBase}/images/do-logo.svg`" style="height:100px;"> &nbsp;&nbsp;
-    <img :src="`${assetsBase}/images/labs.jpg`" height="20px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    with support from: <img :src="`${assetsBase}/images/Mellon.jpg`" height="20px">
+    <div class="footer-left">
+      <span>a collaboration between</span>
+      <img :src="`${assetsBase}/images/do-logo.svg`" style="height:25px;"> &nbsp;&nbsp;
+      <img :src="`${assetsBase}/images/labs.jpg`" height="20px">
+    </div>
+
+    <div class="footer-right">
+      <span>with support from: </span>
+      <img :src="`${assetsBase}/images/Mellon.jpg`" height="20px">
+    </div>
   </div>
 </template>
 
 <script>
 
-  module.exports = {  
-    name: 'Footer',
-    props: {
-      siteConfig: { type: Object, default: function(){ return {}} },
-      contentSource:  { type: Object, default: () => ({}) }
-    },
-    data: () => ({}),
-    computed: {
-      maxWidth() { return window.innerWidth },
-      assetsBase() { return this.contentSource.assetsBaseUrl }
-    },
-    mounted() {},
-    methods: {},
-    watch: {}
-  }
+module.exports = {
+  name: 'Footer',
+  props: {
+    siteConfig: { type: Object, default: function(){ return {}} },
+    contentSource:  { type: Object, default: () => ({}) }
+  },
+  data: () => ({}),
+  computed: {
+    maxWidth() { return window.innerWidth },
+    assetsBase() { return this.contentSource.assetsBaseUrl }
+  },
+  mounted() {},
+  methods: {},
+  watch: {}
+}
 
 </script>
 
 <style>
-  
-  [v-cloak] { display: none; }
 
+[v-cloak] { display: none; }
+
+#footer {
+  font-family: Roboto, sans-serif;
+  display: grid;
+  grid-gap: 20vw;
+  grid-area: unset !important;
+  align-items: center;
+  font-size: .8rem;
+  font-weight: bold;
+  padding: 4px 16px;
+}
+
+.footer-left {
+  grid-column: 1/2;
+}
+
+.footer-right {
+  grid-column: 2/3;
+}
+
+#footer img {
+  height: 30px;
+  margin: 0 12px;
+  vertical-align: middle;
+}
+
+@media screen and (max-width: 880px) {
   #footer {
-    font-family: Roboto, sans-serif;
-    display: flex;
-    align-items: center;
-    height: 50px;
-    font-size: .8rem;
-    font-weight: bold;
-    padding-left: 20px;
-  } 
-  #footer img {
-    height: 30px;
-    margin: 0 12px;
+    grid-gap: 40px;
   }
+}
+
+@media screen and (max-width: 770px) {
+  #footer {
+    grid-template-columns: 1fr;
+    grid-gap: 8px;
+    padding: 4px 8px;
+  }
+
+  .footer-left, .footer-right {
+    grid-column: auto;
+  }
+}
 
 </style>
