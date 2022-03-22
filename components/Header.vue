8c542c20aa8def0db356ad854cb71893558e4e51
julia-ha <juliaxha@gmail.com>
2022-03-22 16:06:05 -0400

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -574,9 +574,9 @@
   .title-bar {
     display: grid;
     align-items: stretch;
-    grid-template-rows: 1fr 1fr;
-    grid-template-columns: 2fr 1fr;
-    grid-template-areas: "title resources" "author resources";
+    grid-template:
+      "title resources" 1fr
+      "author resources" 1fr / 2fr 1fr;
     color: white;
     background-color: rgba(0, 0, 0, .6);
     /* padding-top: 14px; */
