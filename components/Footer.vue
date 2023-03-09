5b043dbdee2ff70cd6cbd267b8e9a8e4141183d1
julia-ha <juliaxha@gmail.com>
2023-03-09 16:07:17 -0500

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <div id="footer" :style="`max-width:${maxWidth}px; overflow:hidden;`">
     a collaboration between <img :src="`${assetsBase}/images/do-logo.svg`" style="height:100px;"> &nbsp;&nbsp;
     <img :src="`${assetsBase}/images/labs.jpg`" height="20px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    with support from: <img :src="`${assetsBase}/images/Mellon.jpg`" height="20px">
+    with support from: <img :src="`${assetsBase}/images/Mellon_Logomark_Lockup_Black.png`" height="20px">
   </div>
 </template>
 
